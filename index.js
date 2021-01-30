b6742f5f06a24412782dae93b460c979a242c53c
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-01-30 17:14:18 -0500

    Updated index file with inquirer and response code

--- index.js
+++ index.js
@@ -5,26 +5,80 @@ const CheckboxPrompt = require("inquirer/lib/prompts/checkbox");
 inquirer.prompt([
     {
         type: "input",
-        message: "What is your name?",
-        name: "name"
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is a description of your project?",
+        name: "description"
     },
     {
         type: "checkbox",
-        message: "What languages do you know?",
-        name: "stack",
-        choices: ["HTML", "CSS", "Javascript"]
+        message: "What is included in your table of contents?",
+        name: "contents",
+        choices: ["Installation", "User Stories", "Usage", "Credits", "Features", "Contributing", "Tests"]
+    },
+    {
+        type: "input",
+        message: "How should your project be installed?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Insert user stories.",
+        name: "stories"
+    },
+    {
+        type: "input",
+        message: "How should your project be used?",
+        name: "usage"
     },
     {
         type: "list",
-        message: "What is your preferred method of communication?",
-        name: "contact",
-        choices: ["email", "phone", "telekinesis", "smoke"]
+        message: "What credits do you want to include?",
+        name: "credits",
+        choices: []
     },
+    {
+        type: "list",
+        message: "What features do you want to include?",
+        name: "features",
+        choices: []
+    },
+    {
+        type: "list",
+        message: "Who contributed to your project?",
+        name: "contributors",
+        choices: []
+    },
+    {
+        type: "input",
+        message: "How can users test your project?",
+        name: "tests",
+    },
+
 ]).then(response => {
     const content = `
-# ${response.name}
+# ${response.title}
 
-## Languages
+## ${response.description}
+
+## ${response.contents}
+
+## ${response.install}
+
+## ${response.stories}
+
+## ${response.usage}
+
+## ${response.credits}
+
+## ${response.features}
+
+## ${response.contributors}
+
+## ${response.tests}
     
     `;
     // creates README file
