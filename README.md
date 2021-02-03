319d5b4188fedd31a3a0d9c3fa785cf062a5990c
slisk <63676970+smlisk0630@users.noreply.github.com>
2021-02-03 09:02:18 -0500

    Updated .gitignore and index.js files

--- README.md
+++ README.md
@@ -3,37 +3,25 @@
 ![Issues](https://img.shields.io/github/issues/smlisk0630/weather-dashboard)
 ![Forks](https://img.shields.io/github/forks/smlisk0630/weather-dashboard)
 ![JavaScript 47 Percent](https://img.shields.io/badge/javascript-100%25-yellow)
-
 ## Description
 This repo houses a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package.
-
 ## Link to Deployed Application
 This application can be found at https://smlisk0630.github.io/readme-generator/.
-
 ## Table of Contents
 Installation,User Stories,Usage,Credits,Features,Contributing,Tests
-
 ## Installation
-To install, run npm install
-
+To install, run npm install.
 ## User Stories
 [screenshots]
-
 ## Usage
 To generate a professional README file.
-
 ## Credits
-UNC Coding Bootcamp, Alpha Coder
-
+UNC Coding Bootcamp
 ## Features
 N/A
-
 ## Contributors
 N/A
-
 ## Testing
 N/A
-
 ## License
-MIT License
-    
+![License: MIT License](https://img.shields.io/badge/License-MIT-blue.svg)
--- index.js
+++ index.js
@@ -1,144 +1,116 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const CheckboxPrompt = require("inquirer/lib/prompts/checkbox");
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const renderLicense = license => {
+    // Generates badge for license
+    switch (license) {
+        case 'Apache License 2.0':
+            return '![License: Apache License 2.0](https://img.shields.io/badge/license-Apache-brightgreen.svg)';
+        case 'ISC License':
+            return '![License: ISC License](https://img.shields.io/badge/License-ISC-blueviolet.svg)';
+        case 'MIT License':
+            return '![License: MIT License](https://img.shields.io/badge/License-MIT-blue.svg)';
+        case 'GNU GPLv3':
+            return '![License: GNU GPLv3](https://img.shields.io/badge/License-GNU-ff69b4.svg)';
+        default:
+            console.log('No license found');
+            return '';
+    }
+};
 
 // Inputs for the command line
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "Title"
-    },
-    {
-        type: "input",
-        message: "What is a description of your project?",
-        name: "description"
-    },
-    {
-        type: "checkbox",
-        message: "What is included in your table of contents?",
-        name: "contents",
-        choices: [
-            "Installation",
-            "User Stories",
-            "Usage",
-            "Credits",
-            "Features",
-            "Contributing",
-            "Tests"]
-    },
-    {
-        type: "input",
-        message: "How should your project be installed?",
-        name: "install"
-    },
-    {
-        type: "input",
-        message: "Insert user stories.",
-        name: "stories"
-    },
-    {
-        type: "input",
-        message: "How should your project be used?",
-        name: "usage"
-    },
-    {
-        type: "input",
-        message: "What credits do you want to include?",
-        name: "credits",
-    },
-    {
-        type: "input",
-        message: "What features do you want to include?",
-        name: "features",
-    },
-    {
-        type: "input",
-        message: "Who contributed to your project?",
-        name: "contributors",
-    },
-    {
-        type: "input",
-        message: "How can users test your project?",
-        name: "tests",
-    },
-    {
-        type: "list",
-        message: "Which type of license should your project have?",
-        name: "licenses",
-        choices: ["Apache License 2.0", "ISC License", "MIT License", "GNU GPLv3"]
-    },
-
-]).then(response => {
-    const content = `
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'Title'
+        },
+        {
+            type: 'input',
+            message: 'What is a description of your project?',
+            name: 'description'
+        },
+        {
+            type: 'checkbox',
+            message: 'What is included in your table of contents?',
+            name: 'contents',
+            choices: ['Installation', 'User Stories', 'Usage', 'Credits', 'Features', 'Contributing', 'Tests']
+        },
+        {
+            type: 'input',
+            message: 'How should your project be installed?',
+            name: 'install'
+        },
+        {
+            type: 'input',
+            message: 'Insert user stories.',
+            name: 'stories'
+        },
+        {
+            type: 'input',
+            message: 'How should your project be used?',
+            name: 'usage'
+        },
+        {
+            type: 'input',
+            message: 'What credits do you want to include?',
+            name: 'credits'
+        },
+        {
+            type: 'input',
+            message: 'What features do you want to include?',
+            name: 'features'
+        },
+        {
+            type: 'input',
+            message: 'Who contributed to your project?',
+            name: 'contributors'
+        },
+        {
+            type: 'input',
+            message: 'How can users test your project?',
+            name: 'tests'
+        },
+        {
+            type: 'list',
+            message: 'Which type of license should your project have?',
+            name: 'license',
+            choices: ['Apache License 2.0', 'ISC License', 'MIT License', 'GNU GPLv3']
+        }
+    ])
+    .then(response => {
+        const content = `
 # ${response.Title}
 ![Issues](https://img.shields.io/github/issues/smlisk0630/weather-dashboard)
 ![Forks](https://img.shields.io/github/forks/smlisk0630/weather-dashboard)
 ![JavaScript 47 Percent](https://img.shields.io/badge/javascript-100%25-yellow)
-
 ## Description
 ${response.description}
-
 ## Link to Deployed Application
 This application can be found at https://smlisk0630.github.io/readme-generator/.
-
 ## Table of Contents
 ${response.contents}
-
 ## Installation
 ${response.install}
-
 ## User Stories
 ${response.stories}
-
 ## Usage
 ${response.usage}
-
 ## Credits
 ${response.credits}
-
 ## Features
 ${response.features}
-
 ## Contributors
 ${response.contributors}
-
 ## Testing
 ${response.tests}
-
 ## License
-${response.licenses}
-    `;
+${renderLicense(response.license)}`;
 
-    // creates README file
-    fs.writeFile("README.md", content, err => {
-        if (err) console.log(err);
-        else console.log("success!");
+        // creates README file
+        fs.writeFile('README.md', content, err => {
+            if (err) console.log(err);
+            else console.log('success!');
+        });
     });
-
-    // Generates badge for license
-    switch (response.licenses) {
-        case "Apache License 2.0":
-            return '[![License: Apache License 2.0]("https://img.shields.io/badge/license-Apache-brightgreen.svg")]';
-        case "ISC License":
-            return '[![License: ISC License]("https://img.shields.io/badge/License-ISC-blueviolet.svg")]';
-        case "MIT License":
-            return '[![License: MIT License]("https://img.shields.io/badge/License-MIT-blue.svg")]';
-        case "GNU GPLv3":
-            return '[![License: GNU GPLv3]("https://img.shields.io/badge/License-GNU-ff69b4.svg")]';
-        default:
-            console.log("No license found");
-    }
-
-    process.on('unhandledRejection', function(err) {
-        console.log(err);
-    });
-
-    (async function() {
-        try {
-            await response.licenses;
-        } catch(err) {
-            console.log(err);
-        }
-    })();
-});
